b5b8b120e5efc1514d599d45f41182e0ce859851
Yusuf Aras <yuzarsif@Yusufs-MacBook-Pro.local>
2024-06-28 21:31:15 +0100

    Adding vercel file

--- src/pages/service/structured-cabling/index.jsx
+++ src/pages/service/structured-cabling/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Wrapper from '../../../layout/wrapper';
-import SEO from '../../../common/seo';
-import Sevice from '../../../components/service';
+import Wrapper from '@/src/layout/wrapper';
+import SEO from '@/src/common/seo';
+import Sevice from '@/src/components/about/pdf';
 
 const index = () => {
     return (
